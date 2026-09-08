9dbae5f0fe04cb97008fc88e9db69774daa5256d
Adarsh Chandran kv <144888670+Adarsh347@users.noreply.github.com>
2026-09-08 11:25:23 +0530

    Change output message in App.java
    Updated the output message in the main method.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdsvsdds");
-        System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdsvsdds");
+        System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdasiydbhaisodjubdsaadsdasvsdds");
     }
 }
