9db60a203d3b31fcef008315fe437f8906cc319f
Adarsh Chandran kv <144888670+Adarsh347@users.noreply.github.com>
2026-09-08 11:34:21 +0530

    Add extra print statement in App class
    Added an additional print statement to the main method.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdsvsdds");
         System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdasiydbhaisodjubdsaadsdasvsdds");
+        System.out.println("Hello from GitHub + Jenkins + Maven!dfvsvsdvdasiydbhaisodjubdsaadsdakjbaslkjdcadssdasvsdds");
     }
 }
